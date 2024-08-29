4a1ff8cf5438121676a90e4daa1d034783f8fa72
Yuriy Dubinin <yuradubinin100@gmail.com>
2024-08-29 19:09:16 +0300

    Delete method

--- cache.go
+++ cache.go
@@ -3,8 +3,8 @@ package cache
 import "fmt"
 
 type Key string
-
 type Value interface{}
+
 type Cache struct {
 	items map[Key]Value
 }
@@ -16,18 +16,20 @@ func New() *Cache {
 }
 
 func (c *Cache) Get(key Key) Value {
-	value, isExist := c.items[key]
+	value, exist := c.items[key]
 
-	if !isExist {
+	if !exist {
 		fmt.Println("Key " + string(key) + " not found.")
 		return nil
 	}
 
-	fmt.Println("The value for the key " + string(key) + " was successfully retrieved.")
 	return value
 }
 
 func (c *Cache) Set(key Key, value Value) {
 	c.items[key] = value
-	fmt.Println("The key " + string(key) + " successfully recorded.")
+}
+
+func (c *Cache) Delete(key Key) {
+	delete(c.items, key)
 }
