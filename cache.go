100b6a75b83f4a86e1418ed956979044da7b5bb4
Yuriy Dubinin <yuradubinin100@gmail.com>
2024-08-29 16:32:56 +0300

    Types & Get method

--- cache.go
+++ cache.go
@@ -1,11 +1,27 @@
 package cache
 
+import "fmt"
+
+type Key string
+
+type Value interface{}
 type Cache struct {
-	items map[string]interface{}
+	items map[Key]Value
 }
 
-func New() Cache {
-	return Cache{
-		items: make(map[string]interface{}),
+func New() *Cache {
+	return &Cache{
+		items: make(map[Key]Value),
 	}
+}
+
+func (c *Cache) Get(key Key) Value {
+	var value, isExist = c.items[key]
+
+	if !isExist {
+		fmt.Println("Key not found")
+		return nil
+	}
+
+	return value
 }
